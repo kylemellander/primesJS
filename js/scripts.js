e6e33784c168f569891e2ab96d8ee910545d4c13
Alyssa Horrocks & Kyle Mellander <student@epicodus.com>
2015-09-09 16:39:34 -0700

    Refactor code

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,4 @@
-function factorialArray(num) {
-  var array = []
-
-  if (num >= 2) {
-    for(var i = 2; i <= num; i++) {
-      array.push(i)
-    }
-  }
-  return array;
-};
-
 function primeNumbers(num) {
-  // var array = factorialArray(num);
-  //
-  // for(var i = 0; i < array.length; ++i) {
-  //   var prime = true;
-  //   for(var j = 0; j < i; ++j) {
-  //     if (array[i] % array[j] === 0) {
-  //       prime = false;
-  //     }
-  //   }
-  //   if (prime) {
-  //     primes.push(array[i])
-  //   }
-  // }
-  //
-  // return primes;
   var primes = [];
 
   for(var i = num; i >= 2; --i) {
--- spec/specs.js
+++ spec/specs.js
@@ -1,11 +1,3 @@
-describe("factorialArray", function() {
-
-  it("returns array of all numbers leading to current number from 2 up", function() {
-    expect(factorialArray(5)).to.eql([2,3,4,5])
-  });
-
-});
-
 describe("primeNumbers", function() {
 
   it("returns array of all primes leading to current number", function() {
